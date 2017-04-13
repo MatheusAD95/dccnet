12e652c72318b2ab4e33e57255bab4c359f65f63
brenocg29 <gabrichbreno@gmail.com>
2017-04-13 14:46:35 -0300

    Early version client ( or server i dont know)

--- client.py
+++ client.py
@@ -1,15 +1,20 @@
 import struct
 import socket
 import sys
-import checksum as errorChk
+import TP2 as errorChk
 #toDo add checksum to frame
 #check if it is right to do data/2
 #change HOST and PORT to spec 
 #figure if the read 1 2 3 4 is always equal 01 02 03 04, we read 1 2 3 4 from file or 01 02 03 04, how to diferentiate 12 from 1 2 
 HOST = '127.0.0.1'
 PORT = 51515
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST,PORT)
+tcp.connect(dest)
+tcp.settimeout(5)
+sync = tcp.recv(1)
 header = 'dcc023c2dcc023c2'
-syncNum = '0'.zfill(4)
+syncNum = str(sync).zfill(4)
 f = open(sys.argv[1],'r')
 data = f.read()
 length =str(len(data)/2)
@@ -20,10 +25,7 @@ length = length.zfill(4)
 preframe =  header+'0000'+length+syncNum+data
 check = errorChk.checksum(preframe)
 frame = header+check+length+syncNum+data
-print frame
-frame = header+length+syncNum+data #add checksum
-dest = (HOST,PORT)
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-
+tcp.send(frame)
+ack = tcp.recv(1)
+print ack
+tcp.close()
